07bf84ff2a98c95dbf5624fd44c6210b9747c5b9
Imad-bitw <imadelkhlifi@gmail.com>
2023-05-04 18:16:23 +0200

    first commit

--- ML1.py
+++ ML1.py
@@ -1,4 +1,5 @@
 from numpy.core.numeric import True_
+from sklearn import metrics
 import matplotlib.pyplot as plt
 import streamlit as st
 import pandas as pd
@@ -57,7 +58,8 @@ def plot_metrics(metrics_list):
 	if "Precision-Recall Curve" in metrics_list:
 		st.subheader("Precision-Recall Curve")
 		PrecisionRecallDisplay.from_estimator(model, x_test, y_test)
-		st.pyplot()
+		st.set_option('deprecation.showPyplotGlobalUse', False)
+		st.pyplot()	
 class_names = ["Age", "Estimated Salary"]
 
 st.sidebar.subheader("Choose classifier")
@@ -68,36 +70,59 @@ if classifier == "Support Vector Machine (SVM)":
 	C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key="C")
 	kernel = st.sidebar.radio("Kernel", ("rbf", "linear"), key="kernel") 
 	gamma = st.sidebar.radio("Gamma (Kernal coefficient", ("scale", "auto"), key="gamma")
-metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
+	metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
 
-if st.sidebar.button("Classify", key="classify"):
-	st.subheader("Support Vector Machine (SVM) results")
-	model = SVC(C=C, kernel=kernel, gamma=gamma)
-	x = df.iloc[:,[2,3]]
-	y = df.iloc[:,4]
-	x_train, x_test,y_train, y_test = train_test_split(x,y, test_size=0.25, random_state=0)
-	model.fit(x_train, y_train)
-	accuracy = model.score(x_test, y_test)
-	y_pred = model.predict(x_test)
-	st.write("Accuracy: ", accuracy.round(2))
-	st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(3))
-	st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(3)) 
-	plot_metrics(metrics)
-
+	if st.sidebar.button("Classify", key="classify"):
+		st.subheader("Support Vector Machine (SVM) results")
+		model = SVC(C=C, kernel=kernel, gamma=gamma)
+		x = df.iloc[:,[2,3]]
+		y = df.iloc[:,4]
+		x_train, x_test,y_train, y_test = train_test_split(x,y, test_size=0.25, random_state=0)
+		model.fit(x_train, y_train)
+		accuracy = model.score(x_test, y_test)
+		y_pred = model.predict(x_test)
+		st.write("Accuracy: ", accuracy.round(2))
+		st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(3))
+		st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(3)) 
+		st.set_option('deprecation.showPyplotGlobalUse', False)
+		plot_metrics(metrics)
 if classifier == "Logistic Regression":
 	st.sidebar.subheader("Hyperparameters")
 	C = st.sidebar.number_input("C (Regularization parameter)", 0.01, 10.0, step=0.01, key="C_LR")
 	max_iter = st.sidebar.slider("Maximum iterations", 100, 500, key="max_iter")
 	metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
-    
 	if st.sidebar.button("Classify", key="classify"):
 		st.subheader("Logistic Regression Results")
 		model = LogisticRegression(C=C, max_iter=max_iter)
+		x = df.iloc[:,[2,3]]
+		y = df.iloc[:,4]
+		x_train, x_test,y_train, y_test = train_test_split(x,y, test_size=0.25, random_state=0)
 		model.fit(x_train, y_train)
 		accuracy = model.score(x_test, y_test)
 		y_pred = model.predict(x_test)
-        
 		st.write("Accuracy: ", accuracy.round(2))
 		st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
 		st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+		st.set_option('deprecation.showPyplotGlobalUse', False)
+		plot_metrics(metrics)
+
+if classifier == "Random Forest":
+	st.sidebar.subheader("Hyperparameters")
+	n_estimators= st.sidebar.number_input("The number of trees in the forest", 100, 5000, step=10, key="n_estimators")
+	max_depth = st.sidebar.number_input("The maximum depth of tree", 1, 20, step =1, key="max_depth")
+	bootstrap = st.sidebar.radio("Bootstrap samples when building trees", ("True", "False"), key="bootstrap")
+	metrics = st.sidebar.multiselect("What metrics to plot?", ("Confusion Matrix", "ROC Curve", "Precision-Recall Curve"))
+	if st.sidebar.button("Classify", key="classify"):
+		st.subheader("Random Forest Results")
+		model = RandomForestClassifier(n_estimators=n_estimators, max_depth=max_depth, bootstrap= False, n_jobs=-1 )
+		x = df.iloc[:,[2,3]]
+		y = df.iloc[:,4]
+		x_train, x_test,y_train, y_test = train_test_split(x,y, test_size=0.25, random_state=0)
+		model.fit(x_train, y_train)
+		accuracy = model.score(x_test, y_test)
+		y_pred = model.predict(x_test)
+		st.write("Accuracy: ", accuracy.round(2))
+		st.write("Precision: ", precision_score(y_test, y_pred, labels=class_names).round(2))
+		st.write("Recall: ", recall_score(y_test, y_pred, labels=class_names).round(2))
+		st.set_option('deprecation.showPyplotGlobalUse', False)
 		plot_metrics(metrics)
